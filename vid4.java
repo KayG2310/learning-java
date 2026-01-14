f4906cfcdd73e14b0a55e6d03380048432bf36e1
KayG2310 <kamakshig2310@gmail.com>
2026-01-14 17:23:25 +0530

    loops

--- vid4.java
+++ vid4.java
@@ -1,5 +1,13 @@
 public class vid4 {
     public static void main(String[] args) {
-        
+        for(int i=0; i<3; i++){
+            System.out.println("HI "+i);
+        }
+        int i = 0;
+        do { 
+            System.out.println(i);
+            i++;
+        } while (i<5);
+        // even if while condition false execute once
     }
 }
