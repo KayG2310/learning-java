96caf6b2f34be0aac756752f2e5ff148fe3cdb14
KayG2310 <kamakshig2310@gmail.com>
2026-01-14 18:08:07 +0530

    functions

---
+++ vid5.java
@@ -0,0 +1,11 @@
+public class vid5 {
+    public static int csum(int a, int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int sum = csum(a,b);
+        System.out.println("The sum is: " + sum);
+    }
+}
